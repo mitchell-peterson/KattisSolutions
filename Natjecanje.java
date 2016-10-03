550c471981e1916912d3a3674069024bd058607c
Mitchell <m_t_peterson@hotmail.com>
2016-10-03 17:58:00 -0500

    new solutions

--- Natjecanje.java
+++ Natjecanje.java
@@ -1,24 +1,27 @@
-import java.util.*;
+import java.io.*;
+
 public class Natjecanje {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); int s = sc.nextInt(); int r = sc.nextInt();
-        int[] teams = new int[n]; 
-        for(int i = 0; i < s; i++) teams[sc.nextInt()-1]--;
-        for(int i = 0; i < r; i++) teams[sc.nextInt()-1]++;
-        for(int i = 0; i < n; i++){
-            if(teams[i] == -1){
-                if(i-1 >= 0 && teams[i-1] == 1){
-                    teams[i]++; teams[i-1]--;
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String[]  line = in.readLine().split(" ");
+        int n = Integer.parseInt(line[0]);
+        int s = Integer.parseInt(line[1]);
+        int r = Integer.parseInt(line[2]);
+        line = in.readLine().split(" ");
+        int[] dmg = new int[n+1];
+        for(int i = 0; i < s; i++) dmg[Integer.parseInt(line[i])]++; //mark them as damaged
+        line = in.readLine().split(" ");
+        for(int i = 0; i < r; i++){
+            int num = Integer.parseInt(line[i]);
+            for(int d = -1; d <= 1 && num+d < dmg.length; d++){ //look one before, this one, and the next one
+                if(dmg[num+d] == 1){ // if this one is damaged
+                    dmg[num+d]--; //mark as undamaged
+                    s--; //how many left damaged
+                    break;
                 }
-                else if(i+1 < n && teams[i+1] == 1){
-                    teams[i]++; teams[i+1]--;
-                }
-                
             }
         }
-        int count = 0;
-        for(int i : teams) if(i == -1) count++;
-        System.out.println(count);
+        System.out.println(s);
+        
     }
 }
